1a8441550245eefc7995378d297c204810721c39
Timamov <timurmov25@gmail.com>
2024-11-24 19:13:55 +0200

    My finally project

--- crud.py
+++ crud.py
@@ -1,8 +1,8 @@
+
 import json
 from enum import Enum
 from typing import Annotated
 
-from fastapi import FastAPI, Query, status
 from fastapi import FastAPI, Query, status, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field
@@ -58,14 +58,11 @@ fake_db_users = [
     },
 ]
 
-
 class User(BaseModel):
     username: str
     is_admin: bool
     token: str
     password: str
-
-
 @app.post('/api/token')
 def token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> dict:
     user_dict = {}
@@ -76,15 +73,11 @@ def token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> dict:
             break
     if not user_dict:
         raise HTTPException(status_code=400, detail='Incorrect username or password')
-
     user = User(**user_dict)
     password = form_data.password
     if password != user.password:
         raise HTTPException(status_code=400, detail='Incorrect username or password')
-
     return {'access_token': user.token, 'token_type': 'bearer'}
-
-
 def get_user_by_token(token: str, is_admin: bool = False) -> User:
     user = None
     for user_data in fake_db_users:
@@ -96,35 +89,28 @@ def get_user_by_token(token: str, is_admin: bool = False) -> User:
             break
     if not user:
         raise HTTPException(status_code=403, detail='Invalid credentials')
-
     return user
-
-
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User | None:
     user = get_user_by_token(token)
     return user
-
-
 def get_current_user_admin(token: Annotated[str, Depends(oauth2_scheme)]) -> User | None:
     user = get_user_by_token(token, is_admin=True)
     return user
 
-
 @app.post('/api/create', status_code=status.HTTP_201_CREATED)
-def create_tour(tour: NewTour, admin_user=Annotated[User, Depends(get_current_user_admin)]) -> SavedTour:
+def create_tour(tour: NewTour, admin_user: Annotated[User, Depends(get_current_user_admin)]) -> SavedTour:
     created_tour = storage.create_tour(json.loads(tour.json()))
     return created_tour
 
 
 @app.get('/api/get-tours/')
-def get_tours(skip: int = Query(default=0, ge=0), limit: int = Query(default=10, gt=0), search: str = '',
-              any_user=None) -> list[SavedTour]:
+def get_tours(any_user: Annotated[User, Depends(get_current_user)], skip: int = Query(default=0, ge=0), limit: int = Query(default=10, gt=0), search: str = '' ) -> list[SavedTour]:
     saved_tours = storage.get_tours(skip, limit, search)
     return saved_tours
 
 
 @app.get('/api/get-tours/{tour_id}')
-def get_tours(tour_id: str, any_user: Annotated[User, Depends(get_current_user)]) -> SavedTour:
+def get_info_tour(tour_id: str, any_user: Annotated[User, Depends(get_current_user)]) -> SavedTour:
     saved_tour = storage.get_info_tour(tour_id)
     return saved_tour
 
@@ -145,3 +131,5 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run('crud:app', reload=True, host='127.0.0.1', port=12000)
+
+
